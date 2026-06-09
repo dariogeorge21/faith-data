c30f886a5baedaa8b1ed139b2e840f694d8a4950
Dario George <edu.dariogeorge21@gmail.com>
2026-06-09 22:54:48 +0530

    add admin dashboard component with basic layout

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminDashboard = () => {
+
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      
+    </div>
+  )
+}
+
+export default AdminDashboard
